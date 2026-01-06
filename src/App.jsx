4ead29ec5de0513e6fd08e1d10fcd9bed3570b39
Baddala Viswanand <viswabaddala16@gmail.com>
2026-01-06 22:48:31 +0530

    feat: implement weather fetching with TanStack React Query

--- src/App.jsx
+++ src/App.jsx
@@ -4,60 +4,62 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+
+
+  const fetchWeather = async (city,apiKey) => {
+
+    if(!city) return null;
+    const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const {data} = await axios.get(BASE_URL, {
+      params: { q: city,appid: apiKey, units: "metric"}
+    });
+    return data;
+
+  }
 
 function App() {
-  const[weather,setWeather] = useState(null);
+  
   const[city,setCity] = useState("");
-  const[loading,setLoading] = useState(false);
-
-  const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-
+  const[searchCity,setSearchCity] = useState("");
   const apiKey= import.meta.env.VITE_API_KEY;
 
+  const{data:weather,isLoading,isError,error} = useQuery({
+    queryKey : ["weather",searchCity],
+    queryFn: () => fetchWeather(searchCity,apiKey),
+    enabled: !!searchCity,
+    retry: false,
+  });
+
+  function handleSearch() {
+    if(city.trim()){
+      setSearchCity(city);
+    }
+  }
   function handleKeyDown(e)  {
     if( e.key === "Enter"){
-      fetchWeather();
+      handleSearch();
     }
+
   };
-  
-  const fetchWeather = async function (){
-
-    if(!city || city.trim() === "") return;
-    console.log("Search weather for:",city);
-    
-    setLoading(true);
-
-    try{
-
-      const response = await axios.get(`${BASE_URL}`, {
-        params: {
-          q: city,
-          appid: apiKey,
-          units: 'metric'
-        }
-      });
-      setWeather(response.data);
-      setCity("");
-
-    }catch(err){
-      if(err.response?.status === 401){
-        alert("Invalid API Key  or Key not yet active");
-      } else {
-        alert("City is Not Found");
-      }
-      loading(false);
-    }
-  }; 
+   
   return (
     < >
       <div className='bg-gradient-to-br from-blue-400 to-indigo-600 rounded-lg '>
         <Header 
         setCity={setCity} 
         city={city} 
-        fetchWeather={fetchWeather}
+        fetchWeather={handleSearch}
         onKeyDown={handleKeyDown}/>
-        {weather && <Main data={weather}/>}
-        {weather && <Footer data={weather}/>}
+        {isLoading && <p className='text-blue-500'>Loading...</p>}
+        {isError && <p className='text-red-700'>Error :  {error.response?.status === 404? "City is not found" : "Something went wrong"}</p>}
+        {weather && 
+          <>
+            <Main data={weather}/>
+            <Footer data={weather}/>
+          </>
+        }
       </div>
       
     </>
